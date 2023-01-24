a45b76e72d46c96c12054ed45a557e8db9bf05ac
Arafat Azam <arafatazam17@gmail.com>
2023-01-24 00:17:03 +0000

    Better way of analyzing before clustering

--- solution.py
+++ solution.py
@@ -7,29 +7,79 @@ World bank data analysis
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from math import ceil
+from textwrap import wrap
+
+YEARS = ['1991', '1995', '1999', '2003', '2007', '2011', '2015', '2019']
+CO2_IDX = 'CO2 emissions (metric tons per capita)'
 
 
-def scatter_matrix(data: pd.DataFrame, country: str):
-    df = data.xs(country, level=1)
-    axs = pd.plotting.scatter_matrix(df.T, figsize=(9, 9), s=20, alpha=0.8)
+def scatter_matrix(data: pd.DataFrame):
+    #Filter by selected years for faster result
+    df = data[YEARS]
+
+    #Unstacking 2nd index (Countries) into columns and then transposing
+    df = df.unstack(level=1).T
+
+    axs = pd.plotting.scatter_matrix(df, figsize=(7, 7), s=20, alpha=0.8)
     for ax in axs.flatten():
         ax.xaxis.label.set_rotation(90)
         ax.yaxis.label.set_rotation(0)
         ax.yaxis.label.set_ha('right')
-    plt.suptitle(f'Scatter Matrix: {country}')
+    plt.suptitle('Scatter Matrix')
     plt.tight_layout()
     plt.show()
 
 
-def main():
-    df = pd.read_excel('wb_data.xlsx', index_col=[0, 1])
-    print(df.index.get_level_values(0).unique())
+def corr_heat_map(data: pd.DataFrame):
+    #Unstacking 2nd index (Countries) into columns and then transposing
+    df = data.unstack(level=1).T
 
-    # In order to find out right candidate for the clustering viewing scatter plot of several countries
-    countries = ['United States', 'United Kingdom',
-                 'China', 'India', 'Bangladesh', 'Nigeria']
-    for country in countries:
-        scatter_matrix(df, country)
+    corr = df.corr()
+    fig, ax = plt.subplots(figsize=(8, 8))
+    im = ax.imshow(corr, interpolation='nearest', cmap='RdYlGn')
+    fig.colorbar(im, orientation='vertical', fraction=0.05)
+    ax.set_xticklabels(['']+["\n".join(wrap(x, 30))
+                       for x in corr.columns.to_list()], rotation=90, fontsize=8)
+    ax.set_yticklabels(['']+["\n".join(wrap(x, 30))
+                       for x in corr.index.to_list()], rotation=0, fontsize=8)
+    for i in range(len(corr.index)):
+        for j in range(len(corr.columns)):
+            text = ax.text(j, i, round(corr.to_numpy()[i, j], 2),
+                           ha="center", va="center", color="black")
+    plt.title('Correlation Heat Map')
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_before_clustering(data: pd.DataFrame, idx: str):
+    df = data.loc[[idx, CO2_IDX], YEARS]
+    df = df.fillna(0.0)
+    col = 3
+    rows = ceil(len(YEARS)/3)
+    iter = 1
+    for year in YEARS:
+        x = df.xs(CO2_IDX, level=0)[[year]]
+        y = df.xs(idx, level=0)[[year]]
+        plt.scatter(x,y,s=5)
+        plt.title(f'{idx} [{year}]')
+        plt.xlabel(CO2_IDX)
+        plt.ylabel(idx)
+        plt.show()
+
+
+def main():
+    raw_data = pd.read_excel('wb_data.xlsx', index_col=[0, 1])
+
+    # Forward fill then backward fill consecutive 3 NaN values. Drop rest of the NaN containingrows 
+    # otherwise they will bias our analysis
+    clean_data = raw_data.ffill(axis='columns', limit=3).bfill(axis='columns', limit=5).dropna(axis='index')
+
+    # Heatmap for choosing appropriate indices for clustering
+    corr_heat_map(clean_data)
+
+    # For getting better insight making scatter matrix
+    scatter_matrix(clean_data)
 
 
 if __name__ == "__main__":
